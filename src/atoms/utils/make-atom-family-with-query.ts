666db7cf4d2ddb4622569774c720df6bed67c3af
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-07-09 17:12:23 -0500

    chore: remove dep on micro-memoize

--- src/atoms/utils/make-atom-family-with-query.ts
+++ src/atoms/utils/make-atom-family-with-query.ts
@@ -1,35 +1,34 @@
 import { atomFamilyWithQuery } from '../atom-family-with-query';
 import type { QueryKey } from 'react-query';
-import type { Scope } from 'jotai/core/atom';
 import type { AtomFamilyWithQueryFn, AtomWithQueryRefreshOptions } from '../types';
 import { AtomFamily } from '../types';
 import { WritableAtom } from 'jotai';
 import { AtomWithQueryAction } from 'jotai/query';
-import memoize from 'micro-memoize';
 
 const cache = new WeakMap();
 
 interface InitOptions<Param, Data> {
   queryKey: QueryKey;
   queryFn: AtomFamilyWithQueryFn<Param, Data>;
-  scope?: Scope;
 }
 
 type Return<Param, Data> = AtomFamily<Param, WritableAtom<Data, AtomWithQueryAction>>;
 
-const _makeAtomFamilyWithQuery = <Param, Data>(initOptions: InitOptions<Param, Data>) =>
-  memoize((options: AtomWithQueryRefreshOptions<Data> = {}): Return<Param, Data> => {
+export const makeAtomFamilyWithQuery = <Param, Data>(initOptions: InitOptions<Param, Data>) => {
+  if (cache.has(initOptions)) return cache.get(initOptions);
+  const fn = (options: AtomWithQueryRefreshOptions<Data> = {}): Return<Param, Data> => {
     const deps = [initOptions, options] as const;
     if (cache.has(deps)) {
       return cache.get(deps);
     }
-    const { queryFn, queryKey, scope, ...defaultOptions } = initOptions;
+    const { queryFn, queryKey, ...defaultOptions } = initOptions;
     const anAtom = atomFamilyWithQuery<Param, Data>(queryKey, queryFn, {
       ...defaultOptions,
       ...options,
     });
     cache.set(deps, anAtom);
     return anAtom;
-  });
-
-export const makeAtomFamilyWithQuery = memoize(_makeAtomFamilyWithQuery);
+  };
+  cache.set(initOptions, fn);
+  return fn;
+};
--- src/atoms/utils/make-atom-with-query.ts
+++ src/atoms/utils/make-atom-with-query.ts
@@ -1,12 +1,15 @@
-import memoize from 'micro-memoize';
-
 import { atomWithQuery } from '../atom-with-query';
 import type { AtomWithQueryRefreshOptions, AtomWithQueryFn } from '../types';
 import type { QueryKey } from 'react-query';
 
-const fn = <Data>(queryKey: QueryKey, queryFn: AtomWithQueryFn<Data>) =>
-  memoize((options: AtomWithQueryRefreshOptions<Data> = {}) =>
-    atomWithQuery<Data>(queryKey, queryFn, options)
-  );
-
-export const makeAtomWithQuery = memoize(fn);
+const cache = new WeakMap();
+export const makeAtomWithQuery = <Data>(initOptions: {
+  queryKey: QueryKey;
+  queryFn: AtomWithQueryFn<Data>;
+}) => {
+  if (cache.has(initOptions)) return cache.get(initOptions);
+  const fn = (options: AtomWithQueryRefreshOptions<Data> = {}) =>
+    atomWithQuery<Data>(initOptions.queryKey, initOptions.queryFn, options);
+  cache.set(initOptions, fn);
+  return fn;
+};
