5506d46b864827d257a3b09492630278d105a22b
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-08-08 09:37:29 -0500

    fix: better ssr conditional (support react-native)

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const IS_SSR = typeof document === 'undefined';
+export const IS_SSR = typeof window === 'undefined';
 export const IS_DEV = typeof process === 'object' && process?.env?.NODE_ENV !== 'production';
 export const QueryRefreshRates: Record<'Default' | 'Fast' | 'RealTime' | 'None', number | false> = {
   Default: 10_000,
