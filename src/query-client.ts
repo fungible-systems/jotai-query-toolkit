86a913963fbcb3b5bbf3c268d6f464f7e5660e0c
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-08-25 14:23:16 -0500

    fix: error message, cache, numbers

--- src/query-client.ts
+++ src/query-client.ts
@@ -3,6 +3,7 @@ import { QueryClient } from 'react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      cacheTime: 1000 * 60 * 60 * 12, // 12 hours
       notifyOnChangeProps: ['data', 'error'],
     },
   },
